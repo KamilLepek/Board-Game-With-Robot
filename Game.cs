6e74594f9edd7c00a432e9f4ba8b2c7a41613820
KamilLepek <kamil.lepek94@gmail.com>
2017-12-09 20:40:06 +0100

    Initial commit. Checking emguCV with C#

---
+++ Game.cs
@@ -0,0 +1,16 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace BoardGameWithRobot
+{
+    public class Game
+    {
+        static void Main(string[] args)
+        {
+            Image<Bgr, byte> img = new Image<Bgr, byte>(@"G:\image.jpg");
+            CvInvoke.Imshow("test", img);
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
